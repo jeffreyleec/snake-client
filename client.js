d150163babf32f1aea2f2941a4a50610d7850824
Jeffrey <jeffrey-lee@live.ca>
2022-08-03 20:00:26 +0000

    exit

--- client.js
+++ client.js
@@ -31,62 +31,11 @@ const connect = function () {
     //   conn.write("Move: right");;
     // }, 9000)
 
-
-
-
-
+    
   
   });
 
-
-  conn.setEncoding("utf8"); // interpret data as text
-  // conn.on("data", (data) => {
-  //console.log("Name: JJJ", data);
-
-  conn.on("data", (key) => {
-    conn.setEncoding("utf8");
-    if (key === '\u0038') { //letter b
-      //process.stdin.write('"Move: up"');   
-      //console.log("Move: up", data)
-      conn.write("Move: up")
-    }
-
-    if (key === '\u0040') { //letter b
-      //process.stdout.write('"Move: down"');  
-      //console.log("Move: down", data)
-      conn.write("Move: down")
-    }
-
-    if (key === '\u0037') { //letter b
-      //process.stdout.write('"Move: left"');  
-      //console.log("Move: left", data) 
-      conn.write("Move: left")
-    }
-
-    if (key === '\u0039') { //letter b
-      //process.stdout.write('"Move: right"');  
-      //console.log("Move: right", data)
-      conn.write("Move: right")
-    }
-
-  });
-
-
-
-
-  //client.write("Name:JJJ");
-
-
-
-
-
-
-
-
-
-
-// interpret incoming data as text
-//conn.setEncoding("utf8");
+  
 
 conn.on("data", (data) => {
   console.log(data.toString());
--- play.js
+++ play.js
@@ -1,4 +1,4 @@
-const {connect} = require("./client");
+const { connect } = require("./client");
 // const net = require("./client");
 
 
@@ -6,7 +6,26 @@ console.log("Connecting ...");
 connect();
 
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+  return stdin;
+
+};
+
+const handleUserInput = function (data) {
+  
+  if ( data === '\u0003') {
+    console.log("Thanks for using me, ciao!")
+    process.exit();
+
+  }
+
+}
 
 
-
-
+setupInput()
