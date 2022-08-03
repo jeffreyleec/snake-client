e274860723e5ac3f6f52c55b0d1bf74aaf58265a
Jeffrey <jeffrey-lee@live.ca>
2022-08-03 18:52:18 +0000

    name and reply

--- client.js
+++ client.js
@@ -1,18 +1,31 @@
+const net = require("net");
+
 // establishes a connection with the game server
-
-const conn = require("./play");
-
-
-
-
 const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541,
+  });
 
+  conn.on("connect", () => {
+    console.log('Successfully connected to game server');
+  });
+
+
+  conn.setEncoding("utf8"); // interpret data as text
+  conn.on("data", (data) => {
+  console.log("Name: JJJ", data);
+  //client.write("Name:JJJ");
+});
+    
   
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
 
-    conn.on("data", (data) => {
+
+  // interpret incoming data as text
+  //conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
     console.log(data.toString());
   });
 
@@ -21,5 +34,6 @@ const connect = function () {
 };
 
 
-module.exports = connect
-module.exports = conn
+module.exports = {
+  connect,
+}
